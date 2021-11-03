da973b68a310cd1fb5654f51d384b3dc07898295
Matheus Borçari Santana <matheus.santana@microcity.com.br>
2021-11-03 19:08:33 -0300

    exercicio 10

--- 10_sort_last.py
+++ 10_sort_last.py
@@ -9,12 +9,41 @@ Irá retornar: [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 
 Dica: Use uma custom key= function para extrair o ultimo elemento de cada tupla.
 """
+
+
+
 def sort_last(tuples):
-    # +++ SUA SOLUÇÃO +++
-    return
+
+    # Usando função própria com sort selection
+    return selection_sort(tuples, key=1)
+
+    # Usando itemgetter
+    #from operator import itemgetter, attrgetter
+    # return sorted(tuples, key=itemgetter(1))
+
+    # Usando sorted com o atritbute key
+    # return sorted(tuples, key=lambda tuple_: tuple_[-1])
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+def selection_sort(tuples, key=None):
+    temp_list = list(tuples)
+    key = 0 if key is None else key
+    for index, item in enumerate(temp_list):
+        minor_item, minor_item_index = item, index
+        flag_swap = False
+        for index_, item_ in enumerate(temp_list[index:]):
+            if item_[key] < minor_item[key]:
+                # soma index_ com index para compensar o slice da lista
+                minor_item, minor_item_index = item_,  index_ + index
+                # Define flag que sinaliza a necesidade de swap
+                flag_swap = True
+        #swap itens
+        if flag_swap:
+            temp_item = temp_list[index]
+            temp_list[index], temp_list[minor_item_index] = minor_item, temp_item
+    return temp_list
+
 
 def test(f, in_, expected):
     """
